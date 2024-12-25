c9087575ee79963e3ab533eb7f8ee48e2443a718
rafi1903139 <66672478+rafi1903139@users.noreply.github.com>
2024-12-25 18:38:06 +0600

    ex 3.6: build a parser function which can parse csv file with or without headers also takes optional arguments to add special capability"

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,41 @@
 # fileparse.py
 #
 # Exercise 3.3
+
+import csv
+
+def parse_csv(filename, select=None, types=None, has_headers=False):
+    'parse a csv file into a list of dictionary'
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        records = []
+
+        if has_headers:
+            headers = next(rows)
+        
+            if not select:
+                select = headers 
+            
+            # get the indices of the selected column
+            indices = [headers.index(colname) for colname in select]
+            
+            for row in rows:
+                if not row: # skip row with no data
+                    continue 
+                
+                if types:        
+                    record = {name: func(row[index]) for func, name, index in zip(types, select, indices)}
+                    records.append(record)
+                else:
+                    record = {name: row[index] for name, index in zip(select, indices)}
+                    records.append(record)
+        else:
+            for row in rows:
+                if types:
+                    record = tuple(func(val) for func, val in zip(types, row))
+                else:
+                    record = tuple(val for val in row)
+
+                records.append(record)
+
+    return records 
