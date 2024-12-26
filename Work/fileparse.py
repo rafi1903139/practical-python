2b9d8dd033beb80b88ee1e2129c6dd4bd0035cc0
rafi1903139 <66672478+rafi1903139@users.noreply.github.com>
2024-12-26 11:53:08 +0600

    ex 3.10: added error handling functionality with try catch block

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -4,11 +4,16 @@
 
 import csv
 
-def parse_csv(filename, select=None, types=None, has_headers=True, delimiter = ','):
+def parse_csv(filename, select=None, types=None, has_headers=True, delimiter = ',', silence_errors=False):
     'parse a csv file into a list of dictionary'
+
+    if select and not has_headers:
+        raise RuntimeError('Select argument requires column headers')
+
     with open(filename, 'rt') as f:
         rows = csv.reader(f, delimiter=delimiter)
         records = []
+       
 
         if has_headers:
             headers = next(rows)
@@ -19,20 +24,35 @@ def parse_csv(filename, select=None, types=None, has_headers=True, delimiter = '
             # get the indices of the selected column
             indices = [headers.index(colname) for colname in select]
             
-            for row in rows:
+            for row_no, row in enumerate(rows, start=1):
+                
+
                 if not row: # skip row with no data
                     continue 
                 
                 if types:        
-                    record = {name: func(row[index]) for func, name, index in zip(types, select, indices)}
-                    records.append(record)
+                    try:
+                        record = {name: func(row[index]) for func, name, index in zip(types, select, indices)}
+                        records.append(record)
+
+                    except ValueError as v:
+                        if not silence_errors:
+                            print(f"Row {row_no}: Couldn't convert {row}")
+                            print(f"Row {row}: Reason {v}")
+
                 else:
                     record = {name: row[index] for name, index in zip(select, indices)}
                     records.append(record)
         else:
             for row in rows:
                 if types:
-                    record = tuple(func(val) for func, val in zip(types, row))
+                    try: 
+                        record = tuple(func(val) for func, val in zip(types, row))
+
+                    except ValueError as v:
+                        if not silence_errors:
+                            print(f"Row {row_no}: Couldn't convert {row}")
+                            print(f"Row {row}: Reason {v}")
                 else:
                     record = tuple(val for val in row)
 
